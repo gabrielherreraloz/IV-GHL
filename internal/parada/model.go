870e6e19eb372926505bad44bfc5ab72ab383e25
chelunike <chelunike@gmail.com>
2025-11-27 23:54:05 +0100

    Añadida lista de correspondencias de lineas con la parada. Tal y como ha indicado @gabrielherreraloz es necesario almacenar esa lista de referencias al código de la línea, que suele ser un número entero, pero hay muchos casos en los que varia y contiene letras. E incluso hay lineas que se referencian con solo letras.

--- internal/parada/model.go
+++ internal/parada/model.go
@@ -4,4 +4,5 @@ type Parada struct {
     nombre string `json:"nombre"`
     latitud float64 `json:"latitud"`
     longitud string `json:"longitud"`
+    lineasCorrespondencia []string `json:"lineasCorrespondencia"`
 }
