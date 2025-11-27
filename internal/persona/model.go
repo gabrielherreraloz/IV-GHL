ed92bfa6bfbdd7e910a16c4ef6d092581c851c88
Gabriel Herrera <gabrielherrera@correo.ugr.es>
2025-11-27 14:12:27 +0100

    FIX-Eliminados archivos del Objetivo 2 para mayor claridad en este, se hará merge cuando se haga el del objetivo 2 para no generar conflictos.

--- internal/persona/model.go
+++
@@ -1,8 +0,0 @@
-package persona
-
-type Persona struct {
-    ID   string `json:"id"`
-    Nombre string `json:"nombre"`
-    Edad int `json:"edad"`
-    PesoMochila float32 `json:"pesoMochila"`
-}
--- internal/transporte/model.go
+++
@@ -1,9 +0,0 @@
-package transporte
-
-type Transporte struct {
-    ID   string `json:"id"`
-    Nombre string `json:"nombre"`
-    coordenadaX float32 `json:"coordenadaX"`
-    coordenadaY float32 `json:"coordenadaY"`
-    vaSentado bool `json:"vaSentado"`
-}
