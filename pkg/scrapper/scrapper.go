621e484f21fc5f39af891e3dd171e7f30ec2994a
Gabriel Herrera <gabrielherrera@correo.ugr.es>
2025-12-13 18:17:14 +0100

    Extracción de lso nombres de paradas(#20)
    Se han creado dos funciones, la función principal toma como parámetro el bloque html donde se encuentra la información, extrae el trozo donde se especifican los nombres, y lo pasa a la segunda función creada, la cual exctrae cada nombre por separado, los almacena todos en un array de string y lo devuelve.

--- pkg/scrapper/scrapper.go
+++ pkg/scrapper/scrapper.go
@@ -13,3 +13,31 @@ func getNumLinea(htmlContent string) string {
 
 	return strings.TrimSpace(partes[0])
 }
+
+func extraerNombresParadas(bloqueHTML string) []string {
+    var nombres []string
+
+    re := regexp.MustCompile(`(?s)<th[^>]*>(.*?)</th>`)
+    matches := re.FindAllStringSubmatch(bloqueHTML, -1)
+
+    for _, match := range matches {
+		nombres = append(nombres, ExtraerTexto(match[1]))
+    }
+
+    return nombres
+}
+
+func ExtraerTexto(celda string) string {
+	reTags := regexp.MustCompile(`<[^>]*>`)
+
+    s := reTags.ReplaceAllString(celda, "")
+    s = strings.ReplaceAll(s, "&nbsp;", "---")
+    s = strings.TrimSpace(s)
+    s = strings.ReplaceAll(s, "\n", "")
+    s = strings.ReplaceAll(s, "\r", "")
+
+    if s == "" || s == "-" {
+        return "---"
+    }
+    return s
+}
